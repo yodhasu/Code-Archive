d018cb9498f28e137146c73ce024cd11674798f4
yodhasu <127666231+yodhasu@users.noreply.github.com>
2024-05-28 18:10:46 +0700

    Add files via upload

---
+++ Java/IntToWord/src/Main.java
@@ -0,0 +1,27 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) throws Exception {
+        english en = new english();
+
+        Scanner input = new Scanner(System.in);
+
+        print("Input a number");
+        String big_numnum = input.nextLine();
+
+        converter(big_numnum, en);
+        input.close();
+    }
+
+    // Function to print like Python
+    public static <P> void print(P var){
+        System.out.println(var);
+    }
+
+    public static void converter(String numnum, english endict){
+        BigInteger biggie;
+        biggie = new BigInteger(numnum);
+        endict.convert(biggie);
+    }
+}
---
+++ Java/IntToWord/src/english.java
@@ -0,0 +1,68 @@
+import java.math.BigInteger;
+
+public class english extends converter{
+
+    private static final String[] tensNames = {
+        "",
+        " ten",
+        " twenty",
+        " thirty",
+        " forty",
+        " fifty",
+        " sixty",
+        " seventy",
+        " eighty",
+        " ninety"
+      };
+    
+      private static final String[] numNames = {
+        "",
+        " one",
+        " two",
+        " three",
+        " four",
+        " five",
+        " six",
+        " seven",
+        " eight",
+        " nine",
+        " ten",
+        " eleven",
+        " twelve",
+        " thirteen",
+        " fourteen",
+        " fifteen",
+        " sixteen",
+        " seventeen",
+        " eighteen",
+        " nineteen"
+      };
+
+      private static final String[] thousandNames = {
+        "",
+        " thousand",
+        " million",
+        " billion",
+        " quadrillion",
+        " quintillion",
+        " sextillion",
+        " septillion",
+        " octillion",
+        " nonillion",
+        " decillion",
+        " undecilion",
+        " duodecillion",
+        " tredecillion",
+        " quattuordecillion",
+        " quindecillion",
+        " sexdecillion",
+        " septendecillion",
+        " octodecillion",
+        " novemdecillion",
+        " vigintillion"
+      };
+      
+      public void convert(BigInteger number){
+        System.out.println(convertToString(number, numNames, tensNames, thousandNames));
+      }
+}
