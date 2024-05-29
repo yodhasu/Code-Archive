24595af070a2553b6a2827af4db44363c6f08729
yodhasu <127666231+yodhasu@users.noreply.github.com>
2024-05-29 16:53:46 +0700

    Update rules.java

--- Java/IntToWord/src/rules.java
+++ Java/IntToWord/src/rules.java
@@ -1,17 +1,10 @@
 public class rules{
     public static String checkRules(String code, String check){
         String result = "";
-        // System.out.println(code);
         switch (code) {
             case "id":
-                // System.out.println(check.contains("satu ribu"));
-                // result = check;
-                if (check.startsWith(" satu ribu") || !check.contains(" ratus")) {
-                    check = check.replace(" satu ribu", " seribu");
-                }
-                check = check.replace(" satu ratus", " seratus");
-                return check.trim();
-                // break;
+                // Input Indonesian language rule
+                break;
         
             default:
                 result = check;
