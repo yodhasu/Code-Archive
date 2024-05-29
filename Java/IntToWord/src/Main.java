3ec1057be7814f20566c7b9c36ce5c966604fd56
yodhasu <127666231+yodhasu@users.noreply.github.com>
2024-05-29 16:51:15 +0700

    Update Main.java

--- Java/IntToWord/src/Main.java
+++ Java/IntToWord/src/Main.java
@@ -1,23 +1,23 @@
 import java.math.BigInteger;
+// import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) throws Exception {
         english en = new english();
-
+        indo id = new indo();
         Scanner input = new Scanner(System.in);
 
         print("Input a number");
+
         try {
             String big_numnum = input.nextLine();
-            converter(big_numnum, en);
+            converter(big_numnum, en, id);
             input.close();
         } catch (Exception e) {
             // TODO: handle exception
             print("Wrong input datatype");
         }
-
-        converter(big_numnum, en);
         input.close();
     }
 
@@ -26,9 +26,11 @@ public class Main{
         System.out.println(var);
     }
 
-    public static void converter(String numnum, english endict){
+    public static void converter(String numnum, english endict, indo iddict){
         BigInteger biggie;
         biggie = new BigInteger(numnum);
         endict.convert(biggie);
+        iddict.convert(biggie);
+        iddict.convert(biggie);
     }
 }
