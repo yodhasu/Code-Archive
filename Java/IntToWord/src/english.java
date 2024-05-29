b51cf72495c0fef8b2b1d3ff2c0a48ffa2ec5ab3
yodhasu <127666231+yodhasu@users.noreply.github.com>
2024-05-29 16:52:01 +0700

    Update english.java

--- Java/IntToWord/src/english.java
+++ Java/IntToWord/src/english.java
@@ -1,9 +1,12 @@
 import java.math.BigInteger;
 
-public class english extends converter{
-    private final String lang = "en"; 
-    
-    private static final String[] tensNames = {
+public class english extends conv{
+  
+    public final String lang = "en";
+
+    public static final String zero = "zero";
+
+    public static final String[] tensNames = {
         "",
         " ten",
         " twenty",
@@ -14,56 +17,60 @@ public class english extends converter{
         " seventy",
         " eighty",
         " ninety"
-      };
-    
-      private static final String[] numNames = {
-        "",
-        " one",
-        " two",
-        " three",
-        " four",
-        " five",
-        " six",
-        " seven",
-        " eight",
-        " nine",
-        " ten",
-        " eleven",
-        " twelve",
-        " thirteen",
-        " fourteen",
-        " fifteen",
-        " sixteen",
-        " seventeen",
-        " eighteen",
-        " nineteen"
-      };
+    };
+  
+    public static final String[] numNames = {
+      "",
+      " one",
+      " two",
+      " three",
+      " four",
+      " five",
+      " six",
+      " seven",
+      " eight",
+      " nine",
+      " ten",
+      " eleven",
+      " twelve",
+      " thirteen",
+      " fourteen",
+      " fifteen",
+      " sixteen",
+      " seventeen",
+      " eighteen",
+      " nineteen"
+    };
 
-      private static final String[] thousandNames = {
-        "",
-        " thousand",
-        " million",
-        " billion",
-        " quadrillion",
-        " quintillion",
-        " sextillion",
-        " septillion",
-        " octillion",
-        " nonillion",
-        " decillion",
-        " undecilion",
-        " duodecillion",
-        " tredecillion",
-        " quattuordecillion",
-        " quindecillion",
-        " sexdecillion",
-        " septendecillion",
-        " octodecillion",
-        " novemdecillion",
-        " vigintillion"
-      };
-      
-      public void convert(BigInteger number){
-        System.out.println(convertToString(lang, number, numNames, tensNames, thousandNames));
-      }
+    public static final String hundreds = " hundred";
+
+    public static final String[] thousandNames = {
+      "",
+      " thousand",
+      " million",
+      " billion",
+      " trilion",
+      " quadrillion",
+      " quintillion",
+      " sextillion",
+      " septillion",
+      " octillion",
+      " nonillion",
+      " decillion",
+      " undecilion",
+      " duodecillion",
+      " tredecillion",
+      " quattuordecillion",
+      " quindecillion",
+      " sexdecillion",
+      " septendecillion",
+      " octodecillion",
+      " novemdecillion",
+      " vigintillion"
+    };
+    public static final boolean hyphen = true;
+    public static final boolean rule = false;
+    public void convert(BigInteger number){
+      System.out.println(convert(number, lang, zero, numNames, tensNames, hundreds, thousandNames, hyphen, rule));
+    }
 }
